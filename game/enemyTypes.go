ded9f1a05f8ec183c1b5a5d64025c4b8a0032274
Patrick Stephen <patrick.d.stephen@gmail.com>
2017-08-09 13:13:02 -0500

    Boomer code, but they don't spawn yet so

--- game/enemyTypes.go
+++ game/enemyTypes.go
@@ -5,7 +5,10 @@ import (
 	"time"
 
 	"github.com/200sc/go-dist/intrange"
+	"github.com/oakmound/oak/event"
+	"github.com/oakmound/oak/physics"
 	"github.com/oakmound/oak/render"
+	"github.com/oakmound/oak/timing"
 )
 
 type EnemyType int
@@ -46,7 +49,7 @@ func NewMelee(x, y int, diff float64) *Enemy {
 	r := render.NewColorBox(8, 8, color.RGBA{120, 120, 120, 255})
 	e := NewEnemy(float64(x*tileDim), float64(y*tileDim), 8, 8, r, 0.2, 5, 0.1, 4)
 	e.Health = 50
-	e.AttackSet = NewAttackSet(intrange.NewLinear(5000, 15000), []float64{1.0}, []*Action{NewAction(SwordDash(Opposing), 0)})
+	e.AttackSet = NewAttackSet(intrange.NewLinear(500, 1500), []float64{1.0}, []*Action{NewAction(SwordDash(Opposing), 0)})
 	e.MoveSet = NewMoveSet([]float64{1.0, 1.0, 0.1, 0.1},
 		Move(Left, 2),
 		Move(Forward, 1),
@@ -60,7 +63,7 @@ func NewRanged(x, y int, diff float64) *Enemy {
 	e := NewEnemy(float64(x*tileDim), float64(y*tileDim), 8, 8, r, 0.2, 5, 0.1, 4)
 	e.Health = 50
 	e.AttackSet = NewAttackSet(intrange.NewLinear(1000, 2000), []float64{1.0},
-		[]*Action{NewAction(Shoot(1, 1, 2, color.RGBA{255, 255, 255, 255}, Opposing, 3*time.Second, .5, 10), 0)})
+		[]*Action{NewAction(Shoot(1, 1, 2, color.RGBA{255, 255, 255, 255}, Opposing, 3*time.Second, .5, 2), 0)})
 	e.MoveSet = NewMoveSet([]float64{1.0, .2, 0.2, 1.0, 2.0},
 		Move(Left, 5),
 		Move(Forward, 5),
@@ -71,7 +74,37 @@ func NewRanged(x, y int, diff float64) *Enemy {
 }
 
 func NewBoomer(x, y int, diff float64) *Enemy {
-	return nil
+	r := render.NewColorBox(12, 12, color.RGBA{200, 120, 120, 255})
+	e := NewEnemy(float64(x*tileDim), float64(y*tileDim), 12, 12, r, 0.2, 20, 0.05, 2)
+	e.Health = 150
+	e.AttackSet = NewAttackSet(intrange.NewLinear(1000, 3000),
+		[]float64{1.0, 1.0},
+		[]*Action{NewAction(SwordLeft(Opposing), 0),
+			NewAction(SwordRight(Opposing), 0)})
+	e.MoveSet = NewMoveSet([]float64{1.0, 1.0, 1.0, 1.0, 1.0},
+		Move(Left, 4),
+		Move(Forward, 1),
+		Move(Forward, 10),
+		Move(Right, 4),
+		Move(Wait, 15))
+	e.Bind(explode, "EnterFrame")
+	return e
+}
+
+func explode(id int, nothing interface{}) int {
+	e := event.GetEntity(id).(*Enemy)
+	if e.Health < 50 {
+		e.Health = 1000
+		go timing.DoAfter(500*time.Millisecond, func() {
+			// todo: Animate explosion
+			for i := 0; i < 360; i += 5 {
+				dir := physics.AngleVector(float64(i))
+				MakeShot(e.Vector, dir, 3, .9, 3, color.RGBA{100, 10, 10, 255}, Opposing, 3*time.Second, .5, 5)
+			}
+			e.Destroy()
+		})
+	}
+	return 0
 }
 
 func NewWizard(x, y int, diff float64) *Enemy {
