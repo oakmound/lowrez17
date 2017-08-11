3789f1396b1202d764ab4ad70f82b8825c94fa3d
Patrick Stephen <patrick.d.stephen@gmail.com>
2017-08-11 14:48:23 -0500

    Added whip

--- game/whip.go
+++ game/whip.go
@@ -1,20 +1,44 @@
 package game
 
-import "github.com/oakmound/oak/collision"
+import (
+	"time"
+
+	"github.com/oakmound/oak/collision"
+	"github.com/oakmound/oak/physics"
+)
 
 func WhipLeft(label collision.Label) func(p *Entity) {
 	return func(p *Entity) {
-
+		out := p.Dir.Copy().Rotate(30)
+		pos := p.CenterPos().Add(out.Copy().Scale(6))
+		fv := physics.NewForceVector(out.Copy(), 20)
+		for i := 0; i < 25; i++ {
+			pos.Add(out)
+			NewHurtBox(pos.X(), pos.Y(), 3, 3, 75*time.Millisecond, label, fv)
+		}
 	}
 }
 func WhipRight(label collision.Label) func(p *Entity) {
 	return func(p *Entity) {
+		out := p.Dir.Copy().Rotate(-30)
+		pos := p.CenterPos().Add(out.Copy().Scale(6))
+		fv := physics.NewForceVector(out.Copy(), 20)
+		for i := 0; i < 25; i++ {
+			pos.Add(out)
+			NewHurtBox(pos.X(), pos.Y(), 3, 3, 75*time.Millisecond, label, fv)
+		}
 
 	}
 }
 
 func WhipTwirl(label collision.Label) func(p *Entity) {
 	return func(p *Entity) {
-
+		rot := p.Dir.Copy().Scale(16)
+		basePos := p.CenterPos()
+		for angle := 0; angle < 360; angle += 10 {
+			pos := basePos.Copy().Add(rot.Rotate(10))
+			fv := physics.NewForceVector(rot.Copy(), 20)
+			NewHurtBox(pos.X(), pos.Y(), 3, 3, 75*time.Millisecond, label, fv)
+		}
 	}
 }
