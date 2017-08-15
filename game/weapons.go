d88905374a222815a865123828c34d1d3d6d68db
StephenPatrick <sythe2o0@gmail.com>
2017-08-14 20:22:15 -0500

    Net left and right animations done

--- game/weapons.go
+++ game/weapons.go
@@ -4,8 +4,10 @@ import (
 	"time"
 
 	"github.com/oakmound/lowrez17/game/forceSpace"
+	"github.com/oakmound/lowrez17/game/layers"
 	"github.com/oakmound/oak/collision"
 	"github.com/oakmound/oak/physics"
+	"github.com/oakmound/oak/render"
 	"github.com/oakmound/oak/timing"
 )
 
@@ -81,6 +83,19 @@ func NetLeft(label collision.Label) func(*Entity) {
 		fv := physics.NewForceVector(p.Dir.Copy().Rotate(180), 3)
 		basePos := p.CenterPos()
 		rot := p.Dir.Copy().Rotate(-130)
+
+		net := render.NewReverting(images["net"].Copy().Modify(render.FlipY))
+		render.Draw(net, layers.DebugLayer)
+		go func(rot physics.Vector) {
+			for a := 0; a < 90; a += 10 {
+				pos := basePos.Copy()
+				rot.Rotate(10)
+				NetRotateAbout(net, pos, basePos, rot.Angle())
+				time.Sleep(20 * time.Millisecond)
+			}
+			net.UnDraw()
+		}(rot.Copy())
+
 		for a := 0; a < 90; a += 10 {
 			pos := basePos.Copy().Add(rot.Copy().Scale(6))
 			forceSpace.NewHurtBox(pos.X(), pos.Y(), 5, 5, 75*time.Millisecond, label, fv)
@@ -94,11 +109,36 @@ func NetRight(label collision.Label) func(*Entity) {
 		fv := physics.NewForceVector(p.Dir.Copy().Rotate(180), 3)
 		basePos := p.CenterPos()
 		rot := p.Dir.Copy().Rotate(130)
+
+		net := render.NewReverting(images["net"].Copy())
+		render.Draw(net, layers.DebugLayer)
+		go func(rot physics.Vector) {
+			for a := 0; a < 90; a += 10 {
+				pos := basePos.Copy()
+				rot.Rotate(-10)
+				NetRotateAbout(net, pos, basePos, rot.Angle())
+				time.Sleep(20 * time.Millisecond)
+			}
+			net.UnDraw()
+		}(rot.Copy())
+
 		for a := 0; a < 90; a += 10 {
 			pos := basePos.Copy().Add(rot.Copy().Scale(6))
 			forceSpace.NewHurtBox(pos.X(), pos.Y(), 5, 5, 75*time.Millisecond, label, fv)
 			rot.Rotate(-10)
 		}
+	}
+}
+func NetRotateAbout(r *render.Reverting, pos, center physics.Vector, angle float64) {
+	r.RevertAndModify(1, render.Rotate(int(-angle)))
+	pos2 := pos.Copy().Add(physics.AngleVector(angle).Scale(2))
+	r.SetPos(pos2.X(), pos2.Y())
+	w, h := r.GetDims()
+	if pos2.X() < center.X()-1 {
+		r.ShiftX(float64(-w))
+	}
+	if pos2.Y() < center.Y()-1 {
+		r.ShiftY(float64(-h))
 	}
 }
 
