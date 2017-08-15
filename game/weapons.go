b1f810e60e13540ee8793192b6105a4268e01ab2
StephenPatrick <sythe2o0@gmail.com>
2017-08-14 20:43:41 -0500

    Spear animations

--- game/weapons.go
+++ game/weapons.go
@@ -45,13 +45,18 @@ const (
 func SpearJab(label collision.Label) func(*Entity) {
 	return func(p *Entity) {
 		fv := physics.NewForceVector(physics.NewVector(0, 0), 0)
-		pos := p.CenterPos().Add(p.Dir.Copy().Scale(15))
+		pos := p.CenterPos().Add(p.Dir.Copy().Scale(20))
 		forceSpace.NewHurtBox(pos.X(), pos.Y(), 7, 7, 75*time.Millisecond, label, fv)
+
+		spear := images["spear"].Copy()
+		RotateAbout(spear, p.CenterPos().Add(p.Dir.Copy().Scale(4)), p.CenterPos(), p.Dir.Angle())
+		render.Draw(spear, layers.DebugLayer)
 
 		stick := collision.NewLabeledSpace(pos.X(), pos.Y(), 7, 7, Stun)
 		collision.Add(stick)
 		go timing.DoAfter(75*time.Millisecond, func() {
 			collision.Remove(stick)
+			spear.UnDraw()
 		})
 	}
 }
@@ -59,13 +64,18 @@ func SpearJab(label collision.Label) func(*Entity) {
 func SpearThrust(label collision.Label) func(*Entity) {
 	return func(p *Entity) {
 		fv := physics.NewForceVector(physics.NewVector(0, 0), 0)
-		pos := p.CenterPos().Add(p.Dir.Copy().Scale(20))
+		pos := p.CenterPos().Add(p.Dir.Copy().Scale(23))
 		forceSpace.NewHurtBox(pos.X(), pos.Y(), 7, 7, 500*time.Millisecond, label, fv)
+
+		spear := images["spear"].Copy()
+		RotateAbout(spear, p.CenterPos().Add(p.Dir.Copy().Scale(5)), p.CenterPos(), p.Dir.Angle())
+		render.Draw(spear, layers.DebugLayer)
 
 		stick := collision.NewLabeledSpace(pos.X(), pos.Y(), 7, 7, Stun)
 		collision.Add(stick)
 		go timing.DoAfter(500*time.Millisecond, func() {
 			collision.Remove(stick)
+			spear.UnDraw()
 		})
 	}
 }
@@ -74,8 +84,15 @@ func SpearDash(label collision.Label) func(*Entity) {
 	return func(p *Entity) {
 		p.Delta.Add(p.Dir.Copy().Scale(24 * p.Speed.Y()))
 		fv := physics.NewForceVector(p.Dir.Copy(), 30)
-		pos := p.CenterPos().Add(p.Dir.Copy().Scale(15))
+		pos := p.CenterPos().Add(p.Dir.Copy().Scale(23))
+
+		spear := images["spear"].Copy()
+		RotateAbout(spear, p.CenterPos().Add(p.Dir.Copy().Scale(5)), p.CenterPos(), p.Dir.Angle())
+		render.Draw(spear, layers.DebugLayer)
+		render.UndrawAfter(spear, 75*time.Millisecond)
+
 		forceSpace.NewHurtBox(pos.X(), pos.Y(), 7, 7, 75*time.Millisecond, label, fv)
+
 	}
 }
 
